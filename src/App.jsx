90577b8cdf647e91b153cb49ad2ac192382014f9
prachi <ugaleprachi789@gmail.com>
2026-08-11 11:19:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CgBell } from "react-icons/cg";
+import { AiFillAmazonCircle } from "react-icons/ai";
+import { AiFillChrome } from "react-icons/ai";
+import { RiBardFill } from "react-icons/ri";
+
+
+const App = () => {
+  return (
+    <div>
+     <CgBell /><br></br>
+    <AiFillAmazonCircle /><br></br>
+     <AiFillChrome /><br></br>
+     <RiBardFill />
+    </div>
+  )
+}
+
+export default App
